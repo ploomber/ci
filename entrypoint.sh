8af9706289abe959c209f4ce60ccb148a2f43ee4
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-05-23 18:51:57 -0400

    configuring conda env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 set -u
@@ -6,6 +6,9 @@ set -x
 
 if [ -f "environment.yml" ]; then
     echo "Setting up conda environment..."
+    conda env create --file environment.yml --force --name env
+    eval "$(conda shell.bash hook)"
+    conda activate env
 else
     echo "environment.yml file does not exist, skipping..."
 fi
@@ -13,8 +16,7 @@ fi
 
 if [ -f "requirements.txt" ]; then
     echo "Installing requirements.txt..."
+    pip install -r requirements.txt
 else
     echo "requirements.txt file does not exist, skipping..."
 fi
-
-conda --help
