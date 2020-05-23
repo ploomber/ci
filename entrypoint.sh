cf4f9040ebc0faa38966f804693461142317d216
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-05-23 18:46:09 -0400

    checking if env files exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,17 @@ set -e
 set -u
 set -x
 
+if [ -f "environment.yml" ]; then
+    echo "Setting up conda environment..."
+else
+    echo "environment.yml file does not exist, skipping..."
+fi
 
-echo "Testing"
+
+if [ -f "requirements.txt" ]; then
+    echo "Installing requirements.txt..."
+else
+    echo "requirements.txt file does not exist, skipping..."
+fi
+
+conda --help
